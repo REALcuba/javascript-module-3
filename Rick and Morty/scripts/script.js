3954718890ff6278e436bdf81ffbbe2fb56e8b4e
REALcuba <angelskcuba@gmail.com>
2022-04-15 22:40:56 +0200

    working on css and fetch episode name

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,11 +1,73 @@
 const root = document.getElementById("root")
 
 // Rick & Morti div
-const ramApiWraper = document.createElement("div");
-ramApiWraper.classList.add("ramApiWraper")
-root.appendChild(ramApiWraper)
+const ramApiWrapper = document.createElement("div");
+ramApiWrapper.classList.add("ramApiWrapper")
+root.appendChild(ramApiWrapper)
 
 const ramApiText = document.createElement("p");
-ramApiWraper.appendChild(ramApiText)
+ramApiWrapper.appendChild(ramApiText)
 ramApiText.classList.add("ramApiText")
 ramApiText.innerText = "Rick and Morti API"
+
+
+// elements Wrapper
+ const elements_wrapper = document.createElement("div");
+ root.appendChild(elements_wrapper)
+ elements_wrapper.id = "elements_wrapper"
+
+ // chapters List div
+const chapters_list = document.createElement("div");
+elements_wrapper.appendChild(chapters_list);
+chapters_list.classList.add("chapters_list")
+
+const chapters_wrapper = document.createElement("div");
+elements_wrapper.appendChild(chapters_wrapper);
+chapters_wrapper.id = "chapters_wrapper";
+
+// create chapters List function  
+
+const episodeOlEl = document.createElement("ol")
+    chapters_list.append(episodeOlEl)
+
+  const episodeLiEl = document.createElement("li")
+    episodeOlEl.appendChild(episodeLiEl)
+    
+
+const createChapters = (episode) => {
+    // episode.forEach(episode => {
+    //     return episodeLiEl(episode.name)}
+    //     )
+    episodeLiEl.innerText = episode.name
+ }
+
+
+const fetchEpisodesName = () => {
+    
+    fetch("https://rickandmortyapi.com/api/episode/1")
+    .then(res => res.json())
+    .then(createChapters)
+    .catch(Error )
+}
+
+function Error() {
+     console.log("not working");
+ }
+fetchEpisodesName()
+
+// create episodes div
+
+const episodesDiv = document.createElement("div")
+chapters_wrapper.appendChild(episodesDiv)
+episodesDiv.id = "episodesDiv"
+
+//create episode div
+
+
+const createEpisodeDiv = () => {
+const episodeDiv = document.createElement("div")
+episodesDiv.appendChild(episodeDiv)
+episodeDiv.classList.add("episodeDiv")
+
+}
+createEpisodeDiv()
